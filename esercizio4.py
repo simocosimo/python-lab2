1098ade2907e9c653e6e970ba95aae5ef9aa56fa
Simone Cosimo <simosx4@gmail.com>
2018-03-26 17:18:59 +0200

    Iniziato es4

---
+++ esercizio4.py
@@ -0,0 +1,7 @@
+task1 = {'todo': 'call John for AmI project organization', 'urgent': True}
+task2 = {'todo': 'buy a new mouse', 'urgent': True}
+task3 = {'todo': 'find a present for Angelina\'s birthday', 'urgent': False}
+task4 = {'todo': 'organize mega party (last week of April)', 'urgent': False}
+task5 = {'todo': 'book summer holidays', 'urgent': False}
+task6 = {'todo': 'whatsapp Mary for a coffee', 'urgent': False}
+
