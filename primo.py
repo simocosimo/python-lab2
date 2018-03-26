8afa54bc94f9c3fbbdf3542f306c07bddf1cccaf
Simone Cosimo <simosx4@gmail.com>
2018-03-26 15:38:28 +0200

    Esercizio tre implementato

--- primo.py
+++ primo.py
@@ -1,14 +1,14 @@
 import sys
 
-todo_list = []
 
-def saveToFile(list):
+def savetofile(lista):
     out_file = open(sys.argv[1], "w")
-    for cosa in list:
+    for cosa in lista:
         out_file.write(cosa + "\n")
     out_file.close()
 
-def loadFromFile():
+
+def loadfromfile():
     in_file = open(sys.argv[1], "r")
     list = in_file.readlines()
     for i in range(0, len(list)):
@@ -16,7 +16,8 @@ def loadFromFile():
     in_file.close()
     return list
 
-todo_list = loadFromFile()
+
+todo_list = loadfromfile()
 while 1:
     choice = int(input("Cosa vuoi fare?\n"
                    "1)Aggiungi una cosa da fare\n"
@@ -24,21 +25,20 @@ while 1:
                    "3)Vedi le cose da fare\n"
                    "4)Chiudi il programma\n"))
 
-    if(choice < 0 or choice > 4):
+    if choice < 0 or choice > 4:
         print("Errore.")
         pass
 
-    if (choice == 1):
+    if choice == 1:
         new = input("Cosa vuoi aggiungere: ")
         todo_list.append(new)
-    elif (choice == 2):
+    elif choice == 2:
         to_delete = input("Inserisci la cosa da cancellare: ")
-        if(to_delete in todo_list):
-            todo_list.remove(to_delete)
-        else:
-            print("Non esiste questa cosa che vuoi fare!")
-    elif (choice == 3):
+        for cosa in todo_list:
+            if to_delete in cosa:
+                todo_list.remove(cosa)
+    elif choice == 3:
         print(sorted(todo_list))
-    elif (choice == 4):
-        saveToFile(todo_list)
+    elif choice == 4:
+        savetofile(todo_list)
         exit(1)
